ebc9d7722fe8c854b00f80da573e48634a6b4265
bk521234 <bryan.kielpinski52@gmail.com>
2018-10-21 19:45:14 -0600

    adding more string compares

--- general_practice/string_functions.go
+++ general_practice/string_functions.go
@@ -19,6 +19,17 @@ func main() {
 	containsAnyFunction()
 }
 
+func equalFunc() {
+	fmt.Println(strings.EqualFold("Australia", "AUSTRALIA"))
+	fmt.Println(strings.EqualFold("Australia", "aUSTRALIA"))
+	fmt.Println(strings.EqualFold("Australia", "Australia"))
+	fmt.Println(strings.EqualFold("Australia", "Aus"))
+	fmt.Println(strings.EqualFold("Australia", "Australia & Japan"))
+	fmt.Println(strings.EqualFold("JAPAN-1254", "japan-1254"))
+	fmt.Println(strings.EqualFold(" ", " "))  // single space both side
+	fmt.Println(strings.EqualFold(" ", "  ")) // double space right side
+}
+
 func containsFunction() {
 	fmt.Println(strings.Contains("Australia", "Aus")) // Any part of string
 	fmt.Println(strings.Contains("Australia", "Australian"))

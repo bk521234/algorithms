9b08551305263ccf3a928886b06104f049530400
bart <bryan.kielpinski52@gmail.com>
2019-12-01 07:52:43 -0700

    updating topological sort file to execute example code only on file execution.

--- python/graph_data_struct/topological_sort.py
+++ python/graph_data_struct/topological_sort.py
@@ -34,18 +34,18 @@ def topological_sort(graph):
 	print(sortedList)
 
 
+if __name__ == "__main__":
+	g = AdjacencyMatrixGraph(9, directed=True)
+	g.add_edge(0, 1)
+	g.add_edge(1, 2)
+	g.add_edge(2, 7)
+	g.add_edge(2, 4)
+	g.add_edge(2, 3)
+	g.add_edge(1, 5)
+	g.add_edge(5, 6)
+	g.add_edge(3, 6)
+	g.add_edge(3, 4)
+	g.add_edge(6, 8)
 
-g = AdjacencyMatrixGraph(9, directed=True)
-g.add_edge(0, 1)
-g.add_edge(1, 2)
-g.add_edge(2, 7)
-g.add_edge(2, 4)
-g.add_edge(2, 3)
-g.add_edge(1, 5)
-g.add_edge(5, 6)
-g.add_edge(3, 6)
-g.add_edge(3, 4)
-g.add_edge(6, 8)
-
-topological_sort(g)
+	topological_sort(g)
 
